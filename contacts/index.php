8371609031e83e56e5f4f869e8badd61bb0246b9
ihptru <ihptru@gmail.com>
2016-11-28 22:00:21 +0300

    contacts

--- contacts/index.php
+++ contacts/index.php
@@ -31,13 +31,16 @@
 
 				<p><b>Адрес:</b> 346703, Ростовская обл, Аксайский р-н, Ленина х, 1081 км. автомагистрали "Дон".</p>
 
-				<p><b>Тел/факс:</b> +7 (863) 203-55-89, +7 (863) 203-55-90, +7 (863) 203-55-91, +7 (863) 203-55-99</p>
+				<p><b>Тел/факс:</b> +7&nbsp;(863)&nbsp;203-55-89, +7&nbsp;(863)&nbsp;203-55-90, +7&nbsp;(863)&nbsp;203-55-91, +7&nbsp;(863)&nbsp;203-55-99</p>
 
 				<p><b>E-mail:</b><br />
-					<b>Генеральный директор</b> – <a href="mailto:gendir@allgreen-russia.ru" title="Написать на эту почту">gendir@all<span class="hidden">is</span>green-russia.ru</a><br />
-					<b>Главный бухгалтер</b> – <a href="mailto:glavbuh@allgreen-russia.ru" title="Написать на эту почту">glavbuh@all<span class="hidden">is</span>green-russia.ru</a><br />
-					<b>Общая почта</b> – <a href="mailto:allgreen@allgreen-russia.ru" title="Написать на общую почту">allgreen@all<span class="hidden">is</span>green-russia.ru</a>
+					<b>Генеральный директор</b> – <a class="mailto" href="mailto:gendir@allgreen-russia.ru" title="Написать на эту почту">gendir@all<span class="hidden">is</span>green-russia.ru</a><br />
+					<b>Главный бухгалтер</b> – <a class="mailto" href="mailto:glavbuh@allgreen-russia.ru" title="Написать на эту почту">glavbuh@all<span class="hidden">is</span>green-russia.ru</a><br />
+					<b>Общая почта</b> – <a class="mailto" href="mailto:allgreen@allgreen-russia.ru" title="Написать на общую почту">allgreen@all<span class="hidden">is</span>green-russia.ru</a>
 				</p>
+
+				<div id="map"></div>
+
 			</div>
 
 
@@ -48,3 +51,37 @@
 <?php
 	include_once("../footer.php");
 ?>
+
+<script>
+
+$(document).ready(function(){
+	ymaps.ready(init);
+	var myMap, 
+	myPlacemark;
+
+	function init(){
+		myMap = new ymaps.Map("map", {
+			center: [39.771537084054394, 47.170569076311104],
+			zoom: 11,
+			type: "yandex#hybrid",
+			controls: []
+		});
+
+		myPlacemark = new ymaps.Placemark([39.81057645749588, 47.093750341111566], {
+			hintContent: '«Алл Грин»',
+			balloonContent: 'Общество с ограниченной ответственностью «Алл Грин»'
+		}, {
+			preset: 'twirl#redDotIcon'
+		});
+
+		myPolyline = new ymaps.Polyline([[39.80722906064529, 47.095860479614515], [39.80727197598953, 47.09298832597525], [39.81006147336501, 47.093867573235656]],
+			{balloonContent: "Схема проезда с трассы М-4"},
+			{strokeColor: "33cc00", strokeWidth: 5, strokeOpacity: 0.8}
+		);
+
+		myMap.geoObjects.add(myPlacemark);
+		myMap.geoObjects.add(myPolyline);
+	}
+});
+
+</script>
--- header.php
+++ header.php
@@ -26,7 +26,7 @@
 
 	<link rel="stylesheet" href="/css/normalize.css">
 	<link rel="stylesheet" href="/css/main.css">
-	<link rel="stylesheet" href="/css/style.css?ver=0.020">
+	<link rel="stylesheet" href="/css/style.css?ver=0.021">
 </head>
 
 <?php
